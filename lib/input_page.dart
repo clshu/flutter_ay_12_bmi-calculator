013ae9cff31dddb1e4679ffedf18bd2b57eed238
clshu <ccss0630@yahoo.com>
2021-06-14 20:04:12 -0700

    Add cardChild property to ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -47,12 +47,14 @@ class _InputPageState extends State<InputPage> {
 
 class ReusableCard extends StatelessWidget {
   final Color color;
+  final Widget cardChild;
 
-  ReusableCard({@required this.color});
+  ReusableCard({@required this.color, this.cardChild});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       height: 200.0,
       width: 170.0,
