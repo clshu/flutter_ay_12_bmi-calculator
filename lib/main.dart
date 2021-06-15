9b623ce8974672d081fe6b586616b51c8676066d
clshu <ccss0630@yahoo.com>
2021-06-14 19:23:44 -0700

    1. Add Container to body. 2. Set height, width, margin,  decoration    color    borderRadius

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
+const bgColor = Color(0xFF0A0121);
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xff2e2d2e),
-        scaffoldBackgroundColor: Color(0xff2e2d2e),
+        primaryColor: bgColor,
+        scaffoldBackgroundColor: bgColor,
       ),
       home: InputPage(),
     );
