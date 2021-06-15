2671e3f500c6aa53d24ad549fef24ccc5118e0c9
clshu <ccss0630@yahoo.com>
2021-06-14 21:20:37 -0700

    1. Rename GenderCard to IconContent. Rename gender to label. 2. Extract ReusableCard and IconContent to different files. 2. Refactor label's TextStyle.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const activeCardColor = Color(0xFF1D1E33);
 
@@ -23,13 +25,13 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     color: activeCardColor,
-                    cardChild: GenderCard(icon: FontAwesomeIcons.mars, gender: "MALE"),
+                    cardChild: IconContent(icon: FontAwesomeIcons.mars, label: "MALE"),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     color: activeCardColor,
-                    cardChild: GenderCard(icon: FontAwesomeIcons.venus, gender: "FEMALE"),
+                    cardChild: IconContent(icon: FontAwesomeIcons.venus, label: "FEMALE"),
                   ),
                 )
               ],
@@ -52,50 +54,3 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class GenderCard extends StatelessWidget {
-  final String gender;
-  final IconData icon;
-
-  GenderCard({@required this.gender, @required this.icon});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, size: 80.0),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          gender,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  final Color color;
-  final Widget cardChild;
-
-  ReusableCard({@required this.color, this.cardChild});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      height: 200.0,
-      width: 170.0,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
