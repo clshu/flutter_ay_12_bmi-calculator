8e77fec2b58ce07d3e27429e8a0cf8c6e1b4ddea
clshu <ccss0630@yahoo.com>
2021-06-15 19:50:26 -0700

    1. Add text to center box. Use Column and Row to arrange layout. Use maxAxisAlignment and crossAlignment to center text and align baseline. 2. Add Slider class. Use SliderTheme to style the slider.

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,11 @@ const kBottomContainerColor = Color(0xFFEB1555);
 const kBottomContainerHeight = 80.0;
 const kActiveCardColor = Color(0xFF1D1E33);
 const kInactiveCardColor = Color(0xFF111320);
-const TextStyle kLabelTextStyle = TextStyle(
+const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98),
+);
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
 );
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,6 +12,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender; // initial value is null
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +32,11 @@ class _InputPageState extends State<InputPage> {
                         selectedGender = Gender.male;
                       });
                     },
-                    color: selectedGender == Gender.male? kActiveCardColor : kInactiveCardColor,
-                    cardChild: IconContent(
-                        icon: FontAwesomeIcons.mars, label: "MALE"),
+                    color: selectedGender == Gender.male
+                        ? kActiveCardColor
+                        : kInactiveCardColor,
+                    cardChild:
+                        IconContent(icon: FontAwesomeIcons.mars, label: "MALE"),
                   ),
                 ),
                 Expanded(
@@ -43,7 +46,9 @@ class _InputPageState extends State<InputPage> {
                         selectedGender = Gender.female;
                       });
                     },
-                    color: selectedGender == Gender.female? kActiveCardColor : kInactiveCardColor,
+                    color: selectedGender == Gender.female
+                        ? kActiveCardColor
+                        : kInactiveCardColor,
                     cardChild: IconContent(
                         icon: FontAwesomeIcons.venus, label: "FEMALE"),
                   ),
@@ -51,7 +56,56 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
             Expanded(
-              child: ReusableCard(color: kActiveCardColor),
+              child: ReusableCard(
+                color: kActiveCardColor,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'HEIGHT',
+                      style: kLabelTextStyle,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: [
+                        Text(
+                          height.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Text(
+                          'cm',
+                          style: kLabelTextStyle,
+                        )
+                      ],
+                    ),
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                          activeTrackColor: Colors.white,
+                          inactiveTrackColor: Color(0xFF8D8E98),
+                          thumbColor: kBottomContainerColor,
+                          overlayColor: Color(0x29EB1555),
+                          thumbShape: RoundSliderThumbShape(
+                            enabledThumbRadius: 15.0,
+                          ),
+                          overlayShape: RoundSliderOverlayShape(
+                            overlayRadius: 30.0,
+                          )),
+                      child: Slider(
+                        value: height.toDouble(),
+                        min: 120.0,
+                        max: 220.0,
+                        onChanged: (double newValue) {
+                          setState(() {
+                            height = newValue.round();
+                          });
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
             Row(
               children: [
